edf386303ce4982803c59dc07a30d94729df742b
Michael Yuen <michael.yuen@ey.com>
2017-11-22 22:00:35 -0500

    fix(app, icon): add imports so unit tests pass

--- src/app/components/icon/icon.component.spec.ts
+++ src/app/components/icon/icon.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { McyIconComponent } from './icon.component';
+import { MatIconModule } from '@angular/material';
 
 describe('IconComponent', () => {
 	let component: McyIconComponent;
@@ -8,6 +9,7 @@ describe('IconComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
+			imports: [ MatIconModule ],
 			declarations: [ McyIconComponent ]
 		})
 		.compileComponents();
