7f73aed080a5dceef039e7c68e1446b9ac8c92bb
Michael Yuen <michael.yuen@ey.com>
2017-11-22 23:04:57 -0500

    chore(travis ci): try example from https://github.com/angular/angular-cli/wiki/stories-continuous-in

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
 		autoWatch: true,
 		browsers: ['Chrome'],
 		customLaunchers: {
-			Chrome_travis_ci: {
+			ChromeNoSandbox: {
 				base: 'Chrome',
 				flags: ['--no-sandbox']
 			}
